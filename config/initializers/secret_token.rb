f42a2165a82bf18c3db764ec77f36c83824ca051
Waqas Mubarik <mwaqas0031@gmail.com>
2014-01-18 22:47:34 +0500

    project initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InamScientific::Application.config.secret_key_base = '93210b7247ad5964a47f4aa14b6783e696eda171207d4124f36acd8772cb443f2b1d53e1f1ba4f9f82a76759cea35e4831492259d1866d39e0206757fe75fb5d'
